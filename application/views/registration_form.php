1b0fdc4b677ef55c6046d035838194f7696587da
Athan972000 <Athan972000@gmail.com>
2015-10-23 14:41:19 +0800

    registration validation

--- application/views/registration_form.php
+++ application/views/registration_form.php
@@ -28,25 +28,31 @@
 							</div>
 							
 							
-							<form action="" method="post" data-toggle="validator">
+							<form id="vocadb_regform" action="" method="post" data-toggle="validator">
 							<div class="panel-body">
 								<div class="form-group has-feedback">
-									<input type="text" type="email" required name="email" id="name" placeholder="email address" class="form-control"/>
+									<input type="text" type="email" name="email" id="name" placeholder="email address" class="form-control" 
+									data-toggle="popover" data-placement="top" data-content="Please enter a valid email address."
+									/>
 									<span class="fa fa-lock form-control-feedback text-muted"></span>
 								</div>
 								<div class="form-group has-feedback">
-									<input pattern="^[a-zA-Z0-9]*$" type="password" required name="password" id="password" placeholder="Password" class="form-control"/>
+									<input type="password" name="password" id="password" placeholder="Password" class="form-control"
+									data-toggle="popover" data-placement="top" data-content="Password atleast 6 Alpha-Numeric characters"
+									/>
 									<span class="fa fa-lock form-control-feedback text-muted"></span>
 								</div>
 								<div class="form-group has-feedback">
-									<input data-match="#password" type="password" required name="repassword" id="repassword" placeholder="Retype Password" class="form-control"/>
+									<input type="password" name="repassword" id="repassword" placeholder="Confirm Password" class="form-control"
+									data-toggle="popover" data-placement="top" data-content="Password do not match"
+									/>
 									<span class="fa fa-lock form-control-feedback text-muted"></span>
 								</div>
 							
 								<div class="clearfix">
 									<div class="checkbox c-checkbox pull-left mt0">
 										<label>
-										   <input type="checkbox" required value="">
+										   <input id="terms" type="checkbox" value="">
 										   <span class="fa fa-check"></span>I agree with the <a href="#">terms</label>							
 									</div>
 								</div>
@@ -61,4 +67,56 @@
 					</div>
 			</div>
 		</div>
+		<script type="text/javascript" src="<?php echo base_url()."resources/vendor/jquery.min.js"; ?>"></script>
+		<script type="text/javascript">
+		$('#vocadb_regform').submit(function (e) {
+			var name = $('#name').val();
+			var emailregex = /.+@.+/;
+			if( !name || !name.match(emailregex) )
+			{
+				e.preventDefault();
+				$('#name').parent('div').addClass("has-error");
+				$('#name').popover('show');
+			}
+			else
+			{
+				$('#name').parent('div').removeClass("has-error");
+			}
+			
+			var pword = $('#password').val();
+			var cpword = $('#repassword').val();
+			if( !pword || !cpword || pword.length < 6 )
+			{
+				e.preventDefault();
+				$('#password').parent('div').addClass("has-error");
+				$('#repassword').parent('div').addClass("has-error");
+				$('#password').popover('show');
+			}
+			else
+			{
+				$('#password').parent('div').removeClass("has-error");
+				$('#repassword').parent('div').removeClass("has-error");
+			}
+			
+			if( pword != cpword )
+			{
+				e.preventDefault();
+				$('#repassword').parent('div').addClass("has-error");
+				$('#repassword').popover('show');
+			}
+			else
+			{
+				$('#repassword').parent('div').removeClass("has-error");
+			}
+			if( !$('#terms').is(':checked') )
+			{
+				e.preventDefault();
+				$('.clearfix ').addClass("has-error");
+			}
+			else
+			{
+				$('.clearfix ').removeClass("has-error");
+			}
+		});
+		</script>
 	</body>
