d49f7138546cf46153582f28bb3e65d418270978
xavalenzuela <xavier.valenzuela@gmail.com>
2015-10-26 17:03:42 +0800

    home page
    icons

--- application/views/home.php
+++ application/views/home.php
@@ -32,7 +32,7 @@
           <span>Translate API</span>
         </h1>
         <p class="subheadline">
-        Quickly and dynamically translate between thousands of available language pairs within your app, integrating with Google Translate.
+        Quickly and dynamically translate between thousands of available language pairs within your app, integrating with VocaDB API.
         </p>
 
         <p class="cta">
@@ -40,11 +40,84 @@
   
     
         <a href="" class="maia-button try-it track-cloud-event" id="try-body" track-type="freeTrial" track-name="try" track-metadata-position="body">Start your free trial</a>
-    
+		
   
 
         </p>
 
       </div>
     </div>
+  </div>
+  
+  <div id="maia-main">
+  <div id="purchase" class="generic-section scroll-section" data-section-label="purchase">
+	<div class="maia-cols">
+        <div class="header">
+          <h2 class="headline maia-col-8"></h2>
+          <p class="subheadline maia-col-8">
+		  <div class="maia-col-6 inner-block">
+		  <p>Visit our developer site to learn more on how VocaDB API works!</p>
+			<a href="" class="btn  btn-default " id="try-body" track-type="freeTrial" track-name="try" track-metadata-position="body">Learn</a>
+		  </div>
+		  <div class="maia-col-6 inner-block">
+			<a href="" class="btn  btn-success " id="try-body" track-type="freeTrial" track-name="try" track-metadata-position="body">Buy</a>
+		  </div>
+			
+			
+          </p>
+        </div>
+      </div>
+  </div>
+  <div id="diagram" class="generic-section scroll-section" data-section-label="diagram">
+		<div class="maia-cols">
+        <div class="header">
+          <h2 class="headline maia-col-8">Applications</h2>
+          <p class="subheadline maia-col-8">
+
+          </p>
+        </div>
+      </div>
+	  
+	<div class="maia-cols">
+
+        <div class="maia-col-8 inner-block">
+          
+          <img src="<?php echo base_url().'resources/images/draw.jpg'?>">
+        </div>
+
+       
+
+        <div class="maia-col-4 inner-block">
+			
+			<p><b><br><br><font color="#990000">[비즈니스]</font></b></p>
+			<p>1. 비즈니스 모델 : B2B, B2C</p> <p>2.
+			비즈니스 분야 : 사전검색분야, 번역서비스분야(휴먼, 기계), OCR 분야, 전자출판사,<br>&nbsp;&nbsp;&nbsp;  Web &amp; App개발사, e-learning, 학원, 언론사, 출판사 (색인생성 및 도서 제작), 영어교육기관, 영어학습자</p> <p>3.
+			서비스 방식 : Open API, 모바일용 SDK, 온라인 서비스, 콘텐츠 서비스</p> <p>&nbsp;</p> 
+		</div>
+    </div>
+		
+	</div>
+  <div id="features" class="generic-section scroll-section" data-section-label="Features">
+      <div class="maia-cols">
+        <div class="header">
+          <h2 class="headline maia-col-8">Features</h2>
+          <p class="subheadline maia-col-8">
+
+          </p>
+        </div>
+      </div>
+      <div class="maia-cols">
+
+        <div class="maia-col-6 inner-block">
+          <h3 class="headline"><img class="inline-icon" src="/images/products/icons/translate-1.png" alt="Dynamic">Dynamically access languages</h3>
+          <p>Google Translate API supports dozens of <a href="/translate/v2/using_rest#language-params" target="_blank" event="autotrack-data-g" data-g-event="Products: Translate API" data-g-action="Features" data-g-label="Languages">languages</a>. Detect a document�s language and translate it using a <a href="/translate/v2/getting_started#REST" target="_blank" track-type="features" track-name="translateRest">RESTful API</a>.</p>
+        </div>
+
+        <div class="maia-col-6 inner-block">
+          <h3 class="headline"><img class="inline-icon" src="/images/products/icons/translate-3.png" alt="Affordable">Affordable, easy pricing</h3>
+          <p>Translate API pricing is based on millions of characters sent. Our <a href="/translate/v2/pricing" target="_blank" track-type="features" track-name="translatePricing">pricing</a> schedule is easy to understand and very affordable.</p>
+        </div>
+      </div>
+    </div>
+	
   </div>
