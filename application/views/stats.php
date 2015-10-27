6c437592f6512e064a43e066bb0c9c9440f58308
xavalenzuela <xavier.valenzuela@gmail.com>
2015-10-27 17:50:52 +0800

    replace highchart with chart.js

--- application/views/stats.php
+++ application/views/stats.php
@@ -5,7 +5,18 @@
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
 <title>Highcharts Example</title>
 <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js"></script>
-<script type="text/javascript">
+
+	</head>
+	<body>
+	
+	<canvas id="myChart" width="500" height="400"></canvas>
+<script src="<?php echo base_url().'resources/js/Chart.js'?>"></script>
+
+<div id="container" style="min-width: 310px; height: 400px; margin: 0 auto"></div>
+
+	</body>
+	
+	<script type="text/javascript">
 var text_result = JSON.parse('<?php echo json_encode($Nvoca['text_result']); ?>');
 
 // console.log(text_result);
@@ -13,55 +24,53 @@ var text_result = JSON.parse('<?php echo json_encode($Nvoca['text_result']); ?>'
 var dateArray = new Array();
 var lengthArray= new Array();
 
-for( i=0; i<text_result.length; i++ )
+for( i=0; i<=text_result.length; i++ )
 {
-	dateArray.push(text_result[i].date);
-	lengthArray.push(text_result[i].length);
+	if(i==0){
+		dateArray.push(text_result[i].date);
+		lengthArray.push(text_result[i].length);
+	}
+	else{
+		for(j=i;j<text_result.length;j++){
+			if(dateArray[i-1]==text_result[j].date){
+				lengthArray[i-1]+=text_result[j].length;
+			}
+			else{
+				dateArray.push(text_result[j].date);
+				lengthArray.push(text_result[j].length);
+				i=j;
+				break;
+			}
+		}
+	}
+	
+	// dateArray.push(text_result[i].date);
+	// lengthArray.push(text_result[i].length);
 }
-
-$(function () {
-	$('#container').highcharts({
-        title: {
-            text: 'Monthly Average Temperature',
-            x: -20 //center
-        },
-        subtitle: {
-            text: 'Source: WorldClimate.com',
-            x: -20
-        },
-        xAxis: {
-            categories: dateArray
-        },
-        yAxis: {
-            title: {
-                text: 'length'
-            },
-            plotLines: [{
-                value: 0,
-                width: 1,
-                color: '#808080'
-            }]
-        },
-       
-        legend: {
-            layout: 'vertical',
-            align: 'right',
-            verticalAlign: 'middle',
-            borderWidth: 0
-        },
-        series: [{
-            name: 'Text',
+console.log(dateArray);
+console.log(lengthArray);
+var data = {
+    labels: dateArray,
+    datasets: [
+        {
+            label: "My First dataset",
+            fillColor: "rgba(151,187,205,0.2)",
+            strokeColor: "rgba(151,187,205,1)",
+            pointColor: "rgba(151,187,205,1)",
+            pointStrokeColor: "#fff",
+            pointHighlightFill: "#fff",
+            pointHighlightStroke: "rgba(220,220,220,1)",
             data: lengthArray
-        }]
-    });
-});
+        },
+        
+    ]
+};
+// Get context with jQuery - using jQuery's .get() method.
+var ctx = $("#myChart").get(0).getContext("2d");
+// This will get the first returned node in the jQuery collection.
+var myNewChart = new Chart(ctx);
+
+new Chart(ctx).Line(data);
+
 		</script>
-	</head>
-	<body>
-<script src="<?php echo base_url().'resources/js/highcharts.js'?>"></script>
-<script src="<?php echo base_url().'resources/js/modules/exporting.js'?>"></script>
-
-<div id="container" style="min-width: 310px; height: 400px; margin: 0 auto"></div>
-
-	</body>
 </html>
